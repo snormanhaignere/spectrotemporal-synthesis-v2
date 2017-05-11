f7945879474fd067fa6c9b20322eee9dbc656d42
Sam Norman-Haignere <snormanhaignere@gmail.com>
2017-05-11 22:32:58 +0200

    Small fix: wav2coch_without_filts.m

--- wav2coch_without_filts.m
+++ wav2coch_without_filts.m
@@ -4,23 +4,24 @@ function [coch, P, R] = wav2coch_without_filts(wav, P)
 % parameter struct
 % 
 % 2016-06-23: Created by Sam NH
-
-duration_sec = length(wav) / P.audio_sr;
+% 
+% 2017-05-11: Very small fix, prior could request non-integer number of samples
+% from the filter
 
 % cochleogram filters
 if P.overcomplete==0
     [audio_filts, audio_low_cutoff] = ...
-        make_erb_cos_filters(duration_sec*P.audio_sr, P.audio_sr, ...
+        make_erb_cos_filters(length(wav), P.audio_sr, ...
         P.n_filts, P.lo_freq_hz, P.audio_sr/2);
     
 elseif P.overcomplete==1
     [audio_filts, audio_low_cutoff] = ...
-        make_erb_cos_filts_double2(duration_sec*P.audio_sr, P.audio_sr, ...
+        make_erb_cos_filts_double2(length(wav), P.audio_sr, ...
         P.n_filts, P.lo_freq_hz, P.audio_sr/2);
     
 elseif P.overcomplete==2
     [audio_filts, audio_low_cutoff] = ...
-        make_erb_cos_filts_quadruple2(duration_sec*P.audio_sr, P.audio_sr, ...
+        make_erb_cos_filts_quadruple2(length(wav), P.audio_sr, ...
         P.n_filts, P.lo_freq_hz, P.audio_sr/2);
 end
 
