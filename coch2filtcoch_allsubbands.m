ef7e43914e363befd7a678612fe65cc3453fecf0
Sam Norman-Haignere <snormanhaignere@gmail.com>
2017-05-24 14:01:16 +0200

    Modified: coch2filtcoch_allsubbands.m
    Made it possible to just compute subbands in the Fourier domain.

--- coch2filtcoch_allsubbands.m
+++ coch2filtcoch_allsubbands.m
@@ -1,7 +1,11 @@
-function filtcoch = coch2filtcoch_allsubbands(coch, P, complex_filters)
+function filtcoch = coch2filtcoch_allsubbands(coch, P, complex_filters, fourier_domain)
 
 if nargin < 3
     complex_filters = false;
+end
+
+if nargin < 4
+    fourier_domain = false;
 end
 
 % FT of the cochleogram
@@ -16,10 +20,14 @@ filtcoch = nan(size(coch,1), size(coch,2), n_filters);
 for i = 1:n_filters
     Hts = filt_spectemp_mod(P.spec_mod_to_match(i), P.temp_mod_to_match(i), ...
         size(coch,2), size(coch,1), P, 0, 0, 0, 0, complex_filters);
-    filtcoch(:,:,i) = ifft2(FT_coch .* Hts);
+    if fourier_domain 
+        filtcoch(:,:,i) = FT_coch .* Hts;
+    else
+        filtcoch(:,:,i) = ifft2(FT_coch .* Hts);
+    end
 end
 
 % ensure real (only needed because of numerical issues)
-if ~complex_filters
+if ~complex_filters && ~fourier_domain
     filtcoch = real(filtcoch);
 end
