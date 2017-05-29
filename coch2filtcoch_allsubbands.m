7f4151e390c8a37704235f4481e112046eaeae09
Sam Norman-Haignere <snormanhaignere@gmail.com>
2017-05-29 15:44:00 +0200

    Improve coch2filtcoch_allsubbands.m
    Separate out temporal and spectral filters

--- coch2filtcoch_allsubbands.m
+++ coch2filtcoch_allsubbands.m
@@ -11,19 +11,18 @@ end
 % FT of the cochleogram
 FT_coch = fft2(coch);
 
-% total number of filters
-assert(length(P.spec_mod_to_match) == length(P.temp_mod_to_match));
-n_filters = length(P.spec_mod_to_match);
-
 % compute the subbands
-filtcoch = nan(size(coch,1), size(coch,2), n_filters);
-for i = 1:n_filters
-    Hts = filt_spectemp_mod(P.spec_mod_to_match(i), P.temp_mod_to_match(i), ...
-        size(coch,2), size(coch,1), P, 0, 0, 0, 0, complex_filters);
-    if fourier_domain 
-        filtcoch(:,:,i) = FT_coch .* Hts;
-    else
-        filtcoch(:,:,i) = ifft2(FT_coch .* Hts);
+filtcoch = nan(size(coch,1), size(coch,2), ...
+    length(P.spec_mod_rates), length(P.temp_mod_rates));
+for i = 1:length(P.spec_mod_rates)
+    for j = 1:length(P.temp_mod_rates)
+        Hts = filt_spectemp_mod(P.spec_mod_rates(i), P.temp_mod_rates(j), ...
+            size(coch,2), size(coch,1), P, 0, 0, 0, 0, complex_filters);
+        if fourier_domain
+            filtcoch(:,:,i,j) = FT_coch .* Hts;
+        else
+            filtcoch(:,:,i,j) = ifft2(FT_coch .* Hts);
+        end
     end
 end
 
