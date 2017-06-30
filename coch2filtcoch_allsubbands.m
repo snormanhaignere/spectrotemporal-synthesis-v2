ace90c658fae988217d4828bc9a7d9bc27f730d6
Sam Norman-Haignere <snormanhaignere@gmail.com>
2017-06-30 11:23:01 +0200

    Updated coch2filtcoch_allsubbands.m

--- coch2filtcoch_allsubbands.m
+++ coch2filtcoch_allsubbands.m
@@ -1,5 +1,9 @@
 function filtcoch = coch2filtcoch_allsubbands(coch, P, complex_filters, fourier_domain)
 
+% Computes filtered cochleograms for all of the model filters.
+% 
+% 2017-06-30: Modified the function to return both negative and positive rates
+
 if nargin < 3
     complex_filters = false;
 end
@@ -13,16 +17,20 @@ FT_coch = fft2(coch);
 
 % compute the subbands
 filtcoch = nan(size(coch,1), size(coch,2), ...
-    length(P.spec_mod_rates), length(P.temp_mod_rates));
+    length(P.spec_mod_rates), length(P.temp_mod_rates), 2);
 for i = 1:length(P.spec_mod_rates)
     for j = 1:length(P.temp_mod_rates)
-        Hts = filt_spectemp_mod(P.spec_mod_rates(i), P.temp_mod_rates(j), ...
-            size(coch,2), size(coch,1), P, P.spec_mod_lowpass(i), ...
-            P.temp_mod_lowpass(j), 0, 0, complex_filters, [], P.causal);
-        if fourier_domain
-            filtcoch(:,:,i,j) = FT_coch .* Hts;
-        else
-            filtcoch(:,:,i,j) = ifft2(FT_coch .* Hts);
+        sgn = [-1 1];
+        for k = 1:2
+            Hts = filt_spectemp_mod(...
+                P.spec_mod_rates(i), sgn(k) * P.temp_mod_rates(j), ...
+                size(coch,2), size(coch,1), P, P.spec_mod_lowpass(i), ...
+                P.temp_mod_lowpass(j), 0, 0, complex_filters, [], P.causal);
+            if fourier_domain
+                filtcoch(:,:,i,j) = FT_coch .* Hts;
+            else
+                filtcoch(:,:,i,j) = ifft2(FT_coch .* Hts);
+            end
         end
     end
 end
