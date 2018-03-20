382c48534bc2656e7ab1b935a75e1c75798e781e
Sam Norman-Haignere <snormanhaignere@gmail.com>
2018-03-20 17:57:39 +0100

    Updated pad_coch_freq.m

--- pad_coch_freq.m
+++ pad_coch_freq.m
@@ -1,12 +1,16 @@
-function padded_coch = pad_coch_freq(coch, P)
+function padded_coch = pad_coch_freq(coch, P, pad_value)
 
 % Adds frequency padding to a cochleogram
+
+if nargin < 3
+    pad_value = mean(coch(:));
+end
 
 % amount of padding to add in samples
 n_freq_pad_smps = round(P.freq_pad_oct / P.logf_spacing);
 
 % matrix set to the mean of the cochleogram
-M = mean(coch(:)) * ones(size(coch,1), n_freq_pad_smps);
+M = pad_value * ones(size(coch,1), n_freq_pad_smps);
 
 % append padding
 padded_coch = [M, coch];
